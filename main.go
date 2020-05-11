d767587ec57854461c8a580f99e8b2b851392644
Adriano <adriano.selaviles@gmail.com>
2020-05-11 12:09:51 -0700

    add markdown processor for markdown RESUME

--- main.go
+++ main.go
@@ -4,27 +4,34 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 )
 
-const (
-	resume      = "resume.pdf"
-	defaultPort = ":80"
-)
-
-func serveFile(file string) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, file)
-	})
-}
+const defaultPort = ":80"
 
 func main() {
-	// When running on Google App Engine, the PORT
+	// when running on Google App Engine, the PORT
 	// env variable is set by the runtime
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
-	err := http.ListenAndServe(port, serveFile(resume))
+
+	// set in app.yaml for Google App Engine,
+	// or in ENV when running locally
+	filename := os.Getenv("FILENAME")
+
+	var h http.Handler
+	switch filepath.Ext(filename) {
+	case ".md":
+		h = mdHandler(filename)
+	case ".pdf":
+		h = fileHandler(filename)
+	default:
+		h = fileHandler(filename)
+	}
+
+	err := http.ListenAndServe(port, h)
 	if err != nil {
 		log.Fatal(err)
 	}
